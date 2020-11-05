6623e882eed96a4fc3f34c3d90b22e775e45db75
juniomelos <juniomelos@gmail.com>
2020-11-05 14:46:55 -0300

    initial commit project -things-backend

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
 
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
+  const characters = strangerThingsService.search(req.query, hereIsTheUpsideDown);
 
   res.status(200).json(characters);
 });
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${PORT}`);
 });

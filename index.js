af3d8f8253f9fa975fc42b86373186679d58635a
juniomelos <juniomelos@gmail.com>
2020-11-05 16:37:17 -0300

    alteraçao .env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -6,6 +7,10 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+// const UPSIDE_DOWN = process.env.UPSIDEDOWN_MODE;
+// const hereIsTheUpsideDown = UPSIDE_DOWN === 'true';
 
 const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
 const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
@@ -20,8 +25,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });

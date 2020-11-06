876646e8dbfd701d460c7478c3a9136736821e78
frederico-campello <fredfmc33@gmail.com>
2020-11-06 16:14:56 -0300

    teste - renato

--- tests/deployHeroku.test.js
+++ tests/deployHeroku.test.js
@@ -3,14 +3,17 @@ const flipout = require('flipout');
 const data = require('../data/dataset/stranger-things-characters.json');
 require('dotenv').config();
 
-const HAWKINS_URL = `https://${process.env.GITHUB_USER}-hawkins-back.herokuapp.com/`
-const UPSIDEDOWN_URL = `https://${process.env.GITHUB_USER}-upside-down-back.herokuapp.com/`
+const HAWKINS_URL = `https://${process.env.GITHUB_USER}-hawkins-back.herokuapp.com/`;
+const UPSIDEDOWN_URL = `https://${process.env.GITHUB_USER}-upside-down-back.herokuapp.com/`;
 
 const flipData = data.map(({ name, origin, status }) => ({
   name: flipout(name),
   origin: flipout(origin),
   status: flipout(status),
 }));
+
+console.log(HAWKINS_URL);
+console.log(UPSIDEDOWN_URL);
 
 describe('Verifica o Deploy no Heroku', () => {
   it('Será validado que ao fazer uma requisição do tipo GET para o endpoint da API Hawkins serão retornadas as informações corretas.', async () => {
@@ -25,16 +28,15 @@ describe('Verifica o Deploy no Heroku', () => {
       });
   });
 
-  it('Será validado que ao fazer uma requisição do tipo GET para o endpoint da API upsideDown serão retornadas as informações corretas',
-    async () => {
-      await frisby
-        .get(UPSIDEDOWN_URL)
-        .expect('status', 200)
-        .then((response) => {
-          const { body } = response;
-          JSON.parse(body).forEach((character) => {
-            expect(flipData).toContainEqual(character);
-          });
+  it('Será validado que ao fazer uma requisição do tipo GET para o endpoint da API upsideDown serão retornadas as informações corretas', async () => {
+    await frisby
+      .get(UPSIDEDOWN_URL)
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        JSON.parse(body).forEach((character) => {
+          expect(flipData).toContainEqual(character);
         });
+      });
   });
 });

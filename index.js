c1a3b4076755f8f5f3352ce04bc885a28b8e11df
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-11-07 14:18:06 -0300

    test wit lecture code

--- index.js
+++ index.js
@@ -39,10 +39,12 @@ const app = express();
 
 // 39502
 const PORT = process.env.PORT || 3000;
-const SERVER_ENV = process.env.SERVER_ENV || 'deu ruim :/';
+// const SERVER_ENV = process.env.SERVER_ENV || 'deu ruim :/';
 
 app.get('/', (_, res) => {
-  res.send(`Você está navegando no ambiente de ${SERVER_ENV}`);
+  res.send(`Você está navegando no ambiente de test`);
+
+  // res.send(`Você está navegando no ambiente de ${SERVER_ENV}`);
 });
 
 app.listen(PORT, () => { console.log(`Escutando na porta ${PORT}`); }); 

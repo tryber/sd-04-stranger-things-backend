00461f3821e8430dcfc77a556045afc4a8c18a7c
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-08 20:40:34 -0300

    Mexi e nada anda

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 // require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(

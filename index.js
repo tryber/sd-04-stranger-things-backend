1f3c3159b2bf4c53c75493d6846e2b4b8adb678f
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-10 18:59:08 -0300

    Changed pra app

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
-// require('dotenv').config();
 
 const port = process.env.PORT;
 const app = express();

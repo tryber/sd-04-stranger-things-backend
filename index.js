04021aa2a0762315729b43f58a8f3908afc61ab2
wistyk <leonardoxw@gmail.com>
2020-11-12 13:35:36 -0300

    teste ainda sem a port configurada localmente

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log('Conectado');
 });

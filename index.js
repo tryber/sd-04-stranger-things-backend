999fcc231749d6fca06d5bcf6892a12d1d4a432c
wistyk <leonardoxw@gmail.com>
2020-11-12 20:25:02 -0300

    push bk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const PORT = process.env.PORT;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -28,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log('Conectado');
 });

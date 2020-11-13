aaf603291b2fcee667b9d8c182cef023d7b353dd
Tiago-ers <trsilva284@gmail.com>
2020-11-13 00:00:39 -0300

    iniciando projeto 04-stranger-things-backend

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const cors = require('cors');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+require('dotenv').config();
 
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+  strangerThingsDataset
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+  strangerThingsRepository
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown
   );
 
   res.status(200).json(characters);

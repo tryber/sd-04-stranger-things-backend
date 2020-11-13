5226d3baa6443132214dbafbaa94a3ff16dd3e18
orlando-messias <programadorlando@gmail.com>
2020-11-13 12:02:44 -0300

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,

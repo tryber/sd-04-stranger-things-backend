89603e88cd6696d37019fbc8df1b6f7230274b85
wistyk <leonardoxw@gmail.com>
2020-11-13 07:21:03 -0300

    restaurado configs originais apenas com o parse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
-const { PORT } = process.env.PORT;
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -10,10 +10,10 @@ const StrangerThingsService = require('./services/StrangerThings');
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+  strangerThingsDataset
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+  strangerThingsRepository
 );
 
 app.use(cors());
@@ -23,7 +23,7 @@ const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown
   );
 
   res.status(200).json(characters);

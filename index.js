1bc14657c58cccb0237f21a4ad901a5f7e1acc5f
danilossousa <danilosantossousa@gmail.com>
2020-11-23 02:00:54 -0300

    v1.0.1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
 });

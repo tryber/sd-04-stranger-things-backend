7824f4d91c3e6426137a2a9fa113eff8bb90aaeb
djonata-dias <diasdjonata@gmail.com>
2020-11-27 15:48:45 -0300

    passei as  variaveis de ambiente para constantes no arquivo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 require('dotenv/config');
 
+const port = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -30,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });

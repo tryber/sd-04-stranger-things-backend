86cf6db3aaf722a672c350e483d7e5a010ce8b5e
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-12-04 18:38:56 -0300

    teste

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const { PORT, UPSIDEDOWN_MODE } = process.env || 3000;
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
@@ -17,19 +16,15 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE || false;
+const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
+  const characters = strangerThingsService.search(req.query, hereIsTheUpsideDown);
 
   res.status(200).json(characters);
 });
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Escutando na porta ${PORT}`);
 });

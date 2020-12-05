f0cae7d3209d56138c296de97337ce5688f0291b
Kaecio <kaecio@pop-os.localdomain>
2020-12-05 19:08:15 -0300

    Criando variavel de ambiente

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+require('dotenv').config();
+
 const strangerThingsDataset = require("./data/dataset/stranger-things-characters.json");
 const StrangerThingsRepository = require("./data/repository/StrangerThings");
 const StrangerThingsService = require("./services/StrangerThings");
@@ -16,8 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.even.UPSIDEDOWM_MODE || "true";
-
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === "true";
 app.get("/", (req, res) => {
   const characters = strangerThingsService.search(
     req.query,

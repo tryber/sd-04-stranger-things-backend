e0a584518bee8206b5500f8c480795186977b43e
Kaecio <kaecio@pop-os.localdomain>
2020-12-05 00:27:54 -0300

    Iniciando o deploy com heroku

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
-const StrangerThingsRepository = require('./data/repository/StrangerThings');
-const StrangerThingsService = require('./services/StrangerThings');
+const strangerThingsDataset = require("./data/dataset/stranger-things-characters.json");
+const StrangerThingsRepository = require("./data/repository/StrangerThings");
+const StrangerThingsService = require("./services/StrangerThings");
 
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+  strangerThingsDataset
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+  strangerThingsRepository
 );
 
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown
   );
 
   res.status(200).json(characters);
 });
 
 app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+  console.log("Escutando na porta 3000");
 });

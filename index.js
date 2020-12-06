956ffe4df7963e3fcd48e8b4aa9baf30ec738012
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-12-05 23:28:38 -0300

    reconfigurando

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -8,12 +7,10 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+const PORT = process.env.PORT || 3000;
+
+const strangerThingsRepository = new StrangerThingsRepository(strangerThingsDataset);
+const strangerThingsService = new StrangerThingsService(strangerThingsRepository);
 
 app.use(cors());
 
@@ -27,7 +24,6 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
